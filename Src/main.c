1ff997c637ac6699b780e106fd419d7edb72bc17
Electron Embedded <electronembedded@gmail.com>
2026-09-18 15:29:52 +0530

    Implement basic LED blinking driver on PC13, closes #1

--- Src/main.c
+++ Src/main.c
@@ -75,10 +75,10 @@ int main(void)
   {
 	  // LED ON
 	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
-	  HAL_Delay(100);
+	  HAL_Delay(300);
 	  // LED OFF
 	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET);
-	  HAL_Delay(100);
+	  HAL_Delay(300);
   }
 
 }
